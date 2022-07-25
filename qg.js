dde80c9542bacd4c122a5b1cc792bfcfa03768b4
sukeerthi-15 <76253205+sukeerthi-15@users.noreply.github.com>
2022-07-25 10:12:40 +0530

    Add files via upload

---
+++ qg.js
@@ -0,0 +1,23 @@
+const text=document.getElementById("quote");
+const author=document.getElementById("author");
+
+const getNewQuote = async () =>
+{
+
+    var url="https://type.fit/api/quotes";    
+
+    const response=await fetch(url);
+    console.log(typeof response);
+
+    const allQuotes = await response.json();
+
+    const indx = Math.floor(Math.random()*allQuotes.length);
+
+    const quote=allQuotes[indx].text;
+
+    const auth=allQuotes[indx].author;
+
+    text.innerHTML=quote;
+    author.innerHTML="~ "+auth;
+}
+getNewQuote();
